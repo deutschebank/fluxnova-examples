1311304fd814cbe44261eabb7833a46a24f962a6
Mannuru, ReddyKishore <ReddyKishore.Mannuru@fmr.com>
2025-10-16 14:38:21 -0600

    Add feature flag for test user creation

--- flowave-springboot-example/src/main/java/org/finos/flowave/UserDataSetup.java
+++ flowave-springboot-example/src/main/java/org/finos/flowave/UserDataSetup.java
@@ -1,13 +1,20 @@
 package org.finos.flowave;
 
 import org.finos.flowave.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 @Component
 public class UserDataSetup implements  CommandLineRunner {
-   private final UserService userService;
+    private static final Logger logger = LoggerFactory.getLogger(UserDataSetup.class);
+    private final UserService userService;
+
+    @Value("${fluxnova.bpm.test-user-setup.enabled:false}")
+    private boolean enableTestUserSetup;
 
     @Autowired
     public UserDataSetup(UserService userService) {
@@ -18,7 +25,14 @@ public class UserDataSetup implements  CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         try {
-            userService.setupDefaultUserGroupAndAuthorizations();
+            logger.info("Test user setup enabled: {}", enableTestUserSetup);
+            if (enableTestUserSetup) {
+                logger.info("Setting up default user group and authorizations.");
+                userService.setupDefaultUserGroupAndAuthorizations();
+                logger.info("Default user group and authorizations setup completed.");
+            } else {
+                logger.info("Test user setup is disabled. Skipping setup.");
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
